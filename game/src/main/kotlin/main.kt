762e8fffbf5270cb598115ce25aad8c856f39664
DESKTOP-UNBSCK8\dexte <Dextervanrenswoude@gmail.com>
2021-06-02 13:21:48 +0200

    added inleiding

--- game/src/main/kotlin/main.kt
+++ game/src/main/kotlin/main.kt
@@ -1,3 +1,15 @@
-fun main(args: Array<String>) {
-    println("commit 1!")
+fun main(args: Array<String>){
+
+
+    println("Enter your character name")
+    var username = readLine()
+
+    while (username!!.isBlank()) {
+        println("please enter a valid username")
+        username = readLine()
+    }
+
+    println("Your username is $username")
+
+    val kamer = Kamer()
 }
---
+++ game1/src/main/kotlin/Enemy.kt
@@ -0,0 +1,6 @@
+open class Enemy(val name: String, var health: Int) {
+
+    fun groet() {
+        println( "hallo, ik ben $name en ik heb $health\n health")
+    }
+}
