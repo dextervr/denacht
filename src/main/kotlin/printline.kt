9efe3017ec229963c52ee2b47204de06259fefd9
Itsrazzx <74185341+raouljoosen1409@users.noreply.github.com>
2021-06-08 15:16:09 +0200

    printline messages

---
+++ src/main/kotlin/printline.kt
@@ -0,0 +1,26 @@
+println("...............................................")
+println("A: Je besluit je ramen en deuren te gaan dubbelchecken in de woonkamer.")
+println("B: Je rent in paniek naar de badkamer en sluit jezelf daar op.")
+println("................................................")
+
+
+println("Je bent aangekomen in de badkamer. Je doet de deur opslot")
+println("Je zie een nagelschaartje liggen op de gootsteen.")
+println("Ook besef je dat als je hier blijft je waarschijnlijk gepakt gaat worden")
+println("wat doe je?")
+
+println("A: Je pakt het nagelschaartje mee en verlaat de kamer.")
+println("B: Je laat het schaartje liggen en verlaat de kamer.")
+println("C: Je blijft in de badkamer schuilen.")
+
+println("Je hebt de badkamer verlaten en staat nu in de gang.")
+println("Je kan 3 kanten op ")
+
+open class Weapon(val name: Jachtgeweer, val damage: 100) {
+    override fun toString(): Jachtgeweer{
+        return "Naam: $name, Damage: $damage"
+    }
+}
+
+
+
